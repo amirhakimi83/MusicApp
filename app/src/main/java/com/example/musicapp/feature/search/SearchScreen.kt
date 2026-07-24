3e7a192074bdb617099a61115ad98e8d41b4f749
amir_hakimi <amir.hkimi45@gmail.com>
2026-07-24 03:40:15 +0330

    fix user bug2

--- app/src/main/java/com/example/musicapp/feature/search/SearchScreen.kt
+++ app/src/main/java/com/example/musicapp/feature/search/SearchScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
